20beebb745637bf86795de9055dd03a3d8190d09
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-07-03 19:00:23 -0400

    Fix message and sizing

--- src/app/pages/@showcase/lazy-loader/lazy-loader.component.ts
+++ src/app/pages/@showcase/lazy-loader/lazy-loader.component.ts
@@ -43,11 +43,10 @@ export class AppModule { }`;
 import { ComponentRegistration } from '@dotglitch/ngx-lazy-loader';
 
 export const RegisteredComponents: ComponentRegistration[] = [
-    // @ae-component-inject
-    { id: 'Landing', load: () => import('src/app/pages/general/landing/landing.component'), hidden: true },
-    { id: 'RegexDiagramGenerator', load: () => import('src/app/pages/@showcase/regex-diagram/regex-diagram.component'), icon: "schema" },
-    { id: 'ContextMenuLibrary', load: () => import('src/app/pages/@showcase/ctx-menu/ctx-menu.component'), icon: "widgets" },
-    { id: 'LazyLoaderLibrary', load: () => import('src/app/pages/@showcase/lazy-loader/lazy-loader.component'), icon: "downloading" }
+    { id: 'Homepage', load: () => import('src/app/pages/landing/landing.component') },
+    { id: 'AboutUs', load: () => import('src/app/pages/about/about.component') },
+    { id: 'ContactUs', load: () => import('src/app/pages/contact/contact.component') },
+    { id: 'MyProfile', load: () => import('src/app/pages/profile/profile.component') }
 ]
 `;
 
