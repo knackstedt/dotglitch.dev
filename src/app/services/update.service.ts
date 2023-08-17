cbddccd82b3a18e0f04db48f27966fe0e796c054
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-08-17 09:16:03 -0400

    🔧chore(update-service) update service checkUpdate method;

--- src/app/services/update.service.ts
+++ src/app/services/update.service.ts
@@ -7,14 +7,38 @@ import { SwUpdate } from '@angular/service-worker';
 })
 export class UpdateService {
     constructor(private swUpdate: SwUpdate, private snackbar: MatSnackBar) {
-        this.swUpdate.versionUpdates.subscribe((evt) => {
-            // Catch installation errors that are not hash mismatches
-            if (evt.type == "VERSION_INSTALLATION_FAILED" && !evt.error.includes("Hash mismatch")) {
-                console.error(evt);
-            }
+        // this.swUpdate.versionUpdates.subscribe((evt) => {
+        //     // Catch installation errors that are not hash mismatches
+        //     if (evt.type == "VERSION_INSTALLATION_FAILED" && !evt.error.includes("Hash mismatch")) {
+        //         console.error(evt);
+        //     }
 
-            // If the new version is ready for reload
-            if (evt.type == "VERSION_READY") {
+        //     // If the new version is ready for reload
+        //     if (evt.type == "VERSION_READY") {
+        //         const snack = this.snackbar.open('Update Available', 'Reload');
+
+        //         snack
+        //             .onAction()
+        //             .subscribe(() => {
+        //                 window.location.reload();
+        //             });
+
+        //         setTimeout(() => {
+        //             snack.dismiss();
+        //         }, 6000);
+        //     }
+        // });
+
+        if (this.swUpdate.isEnabled) {
+            this.checkUpdate();
+        }
+    }
+
+    private timer;
+    async checkUpdate() {
+        try {
+            const isUpdateAvailable = await this.swUpdate.checkForUpdate();
+            if (isUpdateAvailable) {
                 const snack = this.snackbar.open('Update Available', 'Reload');
 
                 snack
@@ -27,9 +51,10 @@ export class UpdateService {
                     snack.dismiss();
                 }, 6000);
             }
-        });
-
-        if (this.swUpdate.isEnabled)
-            this.swUpdate.checkForUpdate();
+        }
+        catch(ex) {
+            // Unclear what errors occur here
+        }
+        setTimeout(() => this.checkUpdate.bind(this), 60 * 1000);
     }
 }
