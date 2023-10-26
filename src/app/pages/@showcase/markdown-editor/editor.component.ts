547d48c193f0bf128c68186d2e3cccc9756f8a46
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-10-26 01:32:53 -0400

    ⚙chore(editor) update page config for fullscreen editor

--- src/app/pages/@showcase/markdown-editor/editor.component.ts
+++ src/app/pages/@showcase/markdown-editor/editor.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { ExampleViewerComponent } from 'src/app/components/example-viewer/example-viewer.component';
 
 import BasicExample from './examples/00-basic';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
     selector: 'app-editor',
     templateUrl: './editor.component.html',
     styleUrls: ['./editor.component.scss'],
     imports: [
-        ExampleViewerComponent
+        ExampleViewerComponent,
+        MatTooltipModule
     ],
     standalone: true
 })
