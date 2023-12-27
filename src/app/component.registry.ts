b13f947be68cb01ec768bc84a79bf8b8e71687e4
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-12-26 22:36:27 -0500

    ⚙chore(registry) update links

--- src/app/component.registry.ts
+++ src/app/component.registry.ts
@@ -6,10 +6,10 @@ export const RegisteredComponents: ComponentRegistration[] = [
     { id: 'RegexDiagramGenerator', load: () => import('src/app/pages/@showcase/regex-diagram/regex-diagram.component'), icon: "schema" },
     { id: 'ContextMenuLibrary', load: () => import('src/app/pages/@showcase/ctx-menu/ctx-menu.component'), icon: "widgets" },
     { id: 'LazyLoaderLibrary', load: () => import('src/app/pages/@showcase/lazy-loader/lazy-loader.component'), icon: "downloading" },
-    { id: 'StackEdit', load: () => import('src/app/pages/@showcase/markdown-editor/editor.component'), icon: "https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsoutlined/markdown/default/24px.svg" },
-    { id: 'ReactFlow', name: "ReactFlow (in Angular)", load: () => import('src/app/pages/@showcase/reactflow-demo/reactflow-demo.component'), icon: "https://reactflow.dev/img/favicon.ico" },
-    { id: 'Excalidraw', name: "Excalidraw (in Angular)", load: () => import('src/app/pages/@showcase/excalidraw-demo/excalidraw-demo.component'), icon: "https://plus.excalidraw.com/favicon-32x32.png" },
+    { id: 'Markdown', load: () => import('src/app/pages/@showcase/markdown-editor/editor.component'), icon: "edit_note" },
+    { id: 'ReactFlow', load: () => import('src/app/pages/@showcase/reactflow-demo/reactflow-demo.component'), hidden: true },
+    { id: 'Excalidraw', load: () => import('src/app/pages/@showcase/excalidraw-demo/excalidraw-demo.component'), hidden: true },
 
-    { id: 'Experiment', load: () => import('src/app/pages/general/experiment/experiment.component'), icon: "beaker", hidden: !window.location.href.includes('localhost') }
+    { id: 'Experiment', load: () => import('src/app/pages/general/experiment/experiment.component'), icon: "experiment", hidden: !window.location.href.includes('localhost') }
 ]
 
